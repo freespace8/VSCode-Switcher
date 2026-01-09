3af5c2d7ef9b6e1ef86fc4fa25a4f7eafefaacf8
freespace8 <f8_soft@qq.com>
2026-01-10 00:50:22 +0800

    Initial Commit

---
+++ VSCode-Switcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VSCode-Switcher
+//
+//  Created by Taylor Ni on 2026/1/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VSCode-Switcher/VSCode_SwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  VSCode_SwitcherApp.swift
+//  VSCode-Switcher
+//
+//  Created by Taylor Ni on 2026/1/10.
+//
+
+import SwiftUI
+
+@main
+struct VSCode_SwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
